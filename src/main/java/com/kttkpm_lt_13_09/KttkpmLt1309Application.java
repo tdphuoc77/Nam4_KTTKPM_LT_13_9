bc964fff50749110f64c963ea10bfa160e5fe8a4
Phuoc <tdphuoc77@gmail.com>
2021-09-13 20:05:10 +0700

    first commit

---
+++ src/main/java/com/kttkpm_lt_13_09/KttkpmLt1309Application.java
@@ -0,0 +1,13 @@
+package com.kttkpm_lt_13_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KttkpmLt1309Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KttkpmLt1309Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kttkpm_lt_13_09/KttkpmLt1309ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kttkpm_lt_13_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KttkpmLt1309ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
